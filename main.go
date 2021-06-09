8773c923e88e8e64079e0a7a391e9feeff1909a0
Appu Goundan <appu@google.com>
2021-06-09 15:10:07 -0400

    whatever

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func main() {
   args := os.Args[1:]
   fmt.Printf("Peace be the journey. %v", args[0])
   fmt.Println()
-  fmt.Printf("::set-output name=time::%v", uuid.New())
+  fmt.Printf("::set-output name=some-output::%v", uuid.New())
   fmt.Println()
 }
